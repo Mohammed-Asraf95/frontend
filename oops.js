2f163a93a8c388ba370ece551e9d7def68f270f5
Mohammed Asraf A <mohammedasraf952005@gmail.com>
2026-06-13 18:48:03 +0530

    Add files via upload

---
+++ oops.js
@@ -0,0 +1,12 @@
+class Student {
+    name = "Asraf";
+    age = 21;
+
+    study() {
+        console.log("Studies");
+    }
+}
+let s = new Student();
+console.log(s.name);
+console.log(s.age);
+s.study();
