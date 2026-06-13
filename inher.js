eb33d1ee2f94e8e8e1021a35890397676468a655
Mohammed Asraf A <mohammedasraf952005@gmail.com>
2026-06-13 18:44:33 +0530

    Add files via upload

---
+++ inher.js
@@ -0,0 +1,23 @@
+class Parent {
+    constructor(name) {
+        this.name = name
+    }
+    marry() {
+        console.log("Marry at 28");
+    }
+}
+class child extends Parent {
+    constructor (name, age) {
+        super(name);
+        this.age = age;
+    } 
+    marry() {
+        super.marry();
+        console.log("Marry at 32")
+    }
+}
+
+let c = new child("Marry, 23");
+console.log(c.name);
+console.log(c.age);
+c.marry()
