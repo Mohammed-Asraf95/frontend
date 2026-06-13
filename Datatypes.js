6adb36847faa2797aca43141a4530aafa18f5bba
Mohammed Asraf A <mohammedasraf952005@gmail.com>
2026-06-13 18:40:46 +0530

    Add files via upload

---
+++ Datatypes.js
@@ -0,0 +1,54 @@
+//Primitive
+//Number
+
+let a=20;
+console.log(typeof a, a);
+
+a = "Asraf";
+console.log(typeof a, a);
+
+let b = 3.14;
+const pi = 3.16;
+console.log(typeof b, b);
+
+str = "Asraf";
+console.log(typeof str, str);
+
+console.log("My name is " + str);
+console.log(`My name is ${str} aged around ${a} , pi value is ${pi}`);
+
+let isvalid=false;
+console.log(typeof isvalid, isvalid);
+
+let name;
+console.log(name);
+
+let z=-10/0;
+console.log(z);
+
+let payment = null;
+console.log(payment);
+
+let g=100n;
+console.log(typeof g, g);
+
+let x = Symbol("user");
+console.log(x);
+console.log(typeof x);
+
+let arr=[1,2,3];
+console.log(arr);
+
+let person = {
+    name: Asraf;
+    age:21;
+};
+console.log(person.name);
+console.log(objects.keys(person));
+console.log(person);
+
+function add() {
+    c = 3 + 3;
+    console.log(c);
+}
+add();
